868b5ae826d69857470dd19f299ec34d4967f4d7
nshpam <pamza2545@gmail.com>
2023-08-07 22:37:38 +0700

    passed autotest

--- ruby-part1/lib/wordguesser_game.rb
+++ ruby-part1/lib/wordguesser_game.rb
@@ -7,6 +7,69 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  attr_accessor :word,:guesses,:wrong_guesses
+
+  def guess(letter)
+
+    # letter is Empty and None
+    if letter.nil? || letter.empty? || letter == ""
+      raise ArgumentError, "Please input the letter."
+    end
+
+    # letter is not a single alphabet
+    if letter.length != 1 then
+      raise ArgumentError, "Please input only one alphabet."
+    end
+
+    # letter is alphabet
+    unless letter.match?(/[A-Za-z]/) then
+      raise ArgumentError, "Please input only alphabet."
+    end
+
+    letter = letter.downcase
+
+    if @word.include?(letter) and not guesses.include?(letter) then
+      @guesses = @guesses+letter
+      return true
+    elsif not @word.include?(letter) and not wrong_guesses.include?(letter) then
+      @wrong_guesses = @wrong_guesses+letter
+      return true
+    end
+    return false
+  end
+
+  def check_str
+    chars = @guesses.split('')
+    chars.each { |c|
+      if not @word.match?(c) then
+        return false
+      end
+    }
+    if @word.length == @guesses.length then
+      return true
+    else
+      return false
+    end
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7 then
+      :lose
+    elsif check_str == true then
+      puts(check_str)
+      :win
+    else
+      :play
+    end
+  end
+
+  def word_with_guesses
+    display = @word.chars.map { |letter| @guesses.include?(letter) ? letter : '-' }
+    display.join('')
   end
 
   # You can test it by installing irb via $ gem install irb
@@ -22,4 +85,13 @@ class WordGuesserGame
     }
   end
 
+
 end
+
+# Check List Created By Sirapa Sangna
+# [/] The computer picks a random word
+# [] The player guesses letters in order to guess the word
+# [] If the player guesses the word before making seven wrong guesses of letters, 
+#    they win; otherwise they lose. (Guessing the same letter repeatedly is simply ignored.)
+# [] A letter that has already been guessed or is a non-alphabet character is considered 
+#    "invalid", i.e. it is not a "valid" guess
